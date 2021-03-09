d676a042450a7b7bccd6e5fcad2cc993c825b8fb
suwisa-yo <suvisa.0413@gmail.com>
2021-03-09 13:52:52 +0700

    index show username

--- add_com.php
+++ add_com.php
@@ -22,58 +22,7 @@
 	$strSQL = "SELECT * FROM user WHERE username = '".$_SESSION['username']."' ";
 	$objQuery = mysqli_query($objCon,$strSQL);
 	$objResult = mysqli_fetch_array($objQuery,MYSQLI_ASSOC);
-	
-if ((isset($_GET['doLogout'])) &&($_GET['doLogout']=="true")){
-	//to fully log out a visitor we need to clear the session varialbles
-  //  $_SESSION['MM_Username'] = NULL;
-  //  $_SESSION['MM_UserGroup'] = NULL;
-  //  $_SESSION['PrevUrl'] = NULL;
-  //  unset($_SESSION['MM_Username']);
-  //  unset($_SESSION['MM_UserGroup']);
-  //  unset($_SESSION['PrevUrl']);
-  session_start();
-	  session_destroy();
-	  header("location:index.php");
-		  
-	$logoutGoTo = "logout.php";
-	if ($logoutGoTo) {
-	  header("Location: $logoutGoTo");
-	  exit;
-	}
-  }
-  if (!function_exists("GetSQLValueString")) {
-  function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
-  {
-	if (PHP_VERSION < 6) {
-	  $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
-	}
-  
-	$theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
-  
-	switch ($theType) {
-	  case "text":
-		$theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
-		break;    
-	  case "long":
-	  case "int":
-		$theValue = ($theValue != "") ? intval($theValue) : "NULL";
-		break;
-	  case "double":
-		$theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
-		break;
-	  case "date":
-		$theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
-		break;
-	  case "defined":
-		$theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
-		break;
-	}
-	return $theValue;
-  }
-  }
-?>
-
-<!DOCTYPE html>
+?><!DOCTYPE html>
 <html lang="en">
 	<head>
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
@@ -134,8 +83,8 @@ if ((isset($_GET['doLogout'])) &&($_GET['doLogout']=="true")){
 				<div class="navbar-header pull-left">
 					<a href="index.html" class="navbar-brand">
 						<small>
-							
-							SIWT
+							<i class="fa fa-leaf"></i>
+							Ace Admin
 						</small>
 					</a>
 				</div>
@@ -224,8 +173,8 @@ if ((isset($_GET['doLogout'])) &&($_GET['doLogout']=="true")){
 							<a data-toggle="dropdown" href="#" class="dropdown-toggle">
 						
 								<span class="user-info">
-									<small>Welcome, <?php echo $row_username['username']; ?></small>
-									
+									<small>Welcome,</small>
+									Jason
 								</span>
 
 								<i class="ace-icon fa fa-caret-down"></i>
@@ -579,10 +528,9 @@ if ((isset($_GET['doLogout'])) &&($_GET['doLogout']=="true")){
 
 						<div class="page-header">
 							<h1>
-								Dashboard
+								Add Computer
 								<small>
-									<i class="ace-icon fa fa-angle-double-right"></i>
-									overview &amp; stats
+							
 								</small>
 							</h1>
 						</div><!-- /.page-header -->
@@ -607,186 +555,8 @@ if ((isset($_GET['doLogout'])) &&($_GET['doLogout']=="true")){
 								<div class="row">
 									<div class="space-6"></div>
 
-									<div class="col-sm-7 infobox-container">
-										<div class="infobox infobox-green">
-											<div class="infobox-icon">
-												<i class="ace-icon fa fa-comments"></i>
-											</div>
+								
 
-											<div class="infobox-data">
-												<span class="infobox-data-number">32</span>
-												<div class="infobox-content">comments + 2 reviews</div>
-											</div>
-
-											<div class="stat stat-success">8%</div>
-										</div>
-
-										<div class="infobox infobox-blue">
-											<div class="infobox-icon">
-												<i class="ace-icon fa fa-twitter"></i>
-											</div>
-
-											<div class="infobox-data">
-												<span class="infobox-data-number">11</span>
-												<div class="infobox-content">new followers</div>
-											</div>
-
-											<div class="badge badge-success">
-												+32%
-												<i class="ace-icon fa fa-arrow-up"></i>
-											</div>
-										</div>
-
-										<div class="infobox infobox-pink">
-											<div class="infobox-icon">
-												<i class="ace-icon fa fa-shopping-cart"></i>
-											</div>
-
-											<div class="infobox-data">
-												<span class="infobox-data-number">8</span>
-												<div class="infobox-content">new orders</div>
-											</div>
-											<div class="stat stat-important">4%</div>
-										</div>
-
-										<div class="infobox infobox-red">
-											<div class="infobox-icon">
-												<i class="ace-icon fa fa-flask"></i>
-											</div>
-
-											<div class="infobox-data">
-												<span class="infobox-data-number">7</span>
-												<div class="infobox-content">experiments</div>
-											</div>
-										</div>
-
-										<div class="infobox infobox-orange2">
-											<div class="infobox-chart">
-												<span class="sparkline" data-values="196,128,202,177,154,94,100,170,224"></span>
-											</div>
-
-											<div class="infobox-data">
-												<span class="infobox-data-number">6,251</span>
-												<div class="infobox-content">pageviews</div>
-											</div>
-
-											<div class="badge badge-success">
-												7.2%
-												<i class="ace-icon fa fa-arrow-up"></i>
-											</div>
-										</div>
-
-										<div class="infobox infobox-blue2">
-											<div class="infobox-progress">
-												<div class="easy-pie-chart percentage" data-percent="42" data-size="46">
-													<span class="percent">42</span>%
-												</div>
-											</div>
-
-											<div class="infobox-data">
-												<span class="infobox-text">traffic used</span>
-
-												<div class="infobox-content">
-													<span class="bigger-110">~</span>
-													58GB remaining
-												</div>
-											</div>
-										</div>
-
-										<div class="space-6"></div>
-
-									
-										
-
-									
-									</div>
-
-									<div class="vspace-12-sm"></div>
-
-									<div class="col-sm-5">
-										<div class="widget-box">
-											<div class="widget-header widget-header-flat widget-header-small">
-												<h5 class="widget-title">
-													<i class="ace-icon fa fa-signal"></i>
-													Traffic Sources
-												</h5>
-
-												<div class="widget-toolbar no-border">
-													<div class="inline dropdown-hover">
-														<button class="btn btn-minier btn-primary">
-															This Week
-															<i class="ace-icon fa fa-angle-down icon-on-right bigger-110"></i>
-														</button>
-
-														<ul class="dropdown-menu dropdown-menu-right dropdown-125 dropdown-lighter dropdown-close dropdown-caret">
-															<li class="active">
-																<a href="#" class="blue">
-																	<i class="ace-icon fa fa-caret-right bigger-110">&nbsp;</i>
-																	This Week
-																</a>
-															</li>
-
-															<li>
-																<a href="#">
-																	<i class="ace-icon fa fa-caret-right bigger-110 invisible">&nbsp;</i>
-																	Last Week
-																</a>
-															</li>
-
-															<li>
-																<a href="#">
-																	<i class="ace-icon fa fa-caret-right bigger-110 invisible">&nbsp;</i>
-																	This Month
-																</a>
-															</li>
-
-															<li>
-																<a href="#">
-																	<i class="ace-icon fa fa-caret-right bigger-110 invisible">&nbsp;</i>
-																	Last Month
-																</a>
-															</li>
-														</ul>
-													</div>
-												</div>
-											</div>
-
-											<div class="widget-body">
-												<div class="widget-main">
-													<div id="piechart-placeholder"></div>
-
-													<div class="hr hr8 hr-double"></div>
-
-													<div class="clearfix">
-														<div class="grid3">
-															<span class="grey">
-																<i class="ace-icon fa fa-facebook-square fa-2x blue"></i>
-																&nbsp; likes
-															</span>
-															<h4 class="bigger pull-right">1,255</h4>
-														</div>
-
-														<div class="grid3">
-															<span class="grey">
-																<i class="ace-icon fa fa-twitter-square fa-2x purple"></i>
-																&nbsp; tweets
-															</span>
-															<h4 class="bigger pull-right">941</h4>
-														</div>
-
-														<div class="grid3">
-															<span class="grey">
-																<i class="ace-icon fa fa-pinterest-square fa-2x red"></i>
-																&nbsp; pins
-															</span>
-															<h4 class="bigger pull-right">1,050</h4>
-														</div>
-													</div>
-												</div><!-- /.widget-main -->
-											</div><!-- /.widget-body -->
-										</div><!-- /.widget-box -->
-									</div><!-- /.col -->
-								</div><!-- /.row -->
 
 								<div class="hr hr32 hr-dotted"></div>
 
